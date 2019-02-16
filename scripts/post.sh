8dbbb0a179680de969e85a9dc55c463b12b03a8e
cappyzawa <cappyzawa@yahoo.ne.jp>
2019-02-17 04:58:18 +0900

    new line

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+echo "title: "
+read title
+
+echo "upload_file: "
+read upload_file
+
+user_id=`curl -s -H "Authorization: Bearer ${MEDIUM_TOKEN}" https://api.medium.com/v1/me | jq .data.id`
+content=`less ${upload_file}`
+data="{\"title\":\"${title}\",\"contentFormat\",\"markdown\",\"publishStatus\":\"draft\",\"content\":\"${content}\"}"
+query="curl -H \"Authorization: Bearer ${MEDIUM_TOKEN}\" -H \"Content-Type: application/json\" -d ${data}"
+eval ${query}
+
